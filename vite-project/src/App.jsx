460408f10f855ec4d7b262b664817cbb2c560cc0
unknown <thorofthunder999-ux>
2026-01-20 10:25:04 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "80px", fontFamily: "Arial" }}>
+        <h1>Hello World</h1>
+        <p>Class Component Demo</p>
+      </div>
+    );
+  }
+}
+
+export default App;
