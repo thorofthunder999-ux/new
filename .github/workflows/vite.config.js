fb9ab08c1b930aaf19b5270c1324175ab337e60d
divyesh-2500031074 <thorofthunder999-ux>
2026-02-01 18:36:26 +0530

    Finalizing GitHub Actions deployment

--- .github/workflows/vite.config.js
+++ .github/workflows/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NEW-REPOSITORY/", 
+  base: '/NEW-REPOSITORY/', // Add this line! Match your repo name exactly.
 })
